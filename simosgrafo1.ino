56c854187724bca29a88a149676a74aab6318f87
Anidroid118 <128540222+Anidroid228@users.noreply.github.com>
2023-07-07 11:48:40 -0500

    Create simosgrafo1.ino
    Primera versión del código

---
+++ simosgrafo1.ino
@@ -0,0 +1,73 @@
+#include <Wire.h> //Incluimos Libreria
+
+const int MPU = 0x68;   //Dirección predeterminada de I2C para MPU 6050
+int16_t AcX, AcY, AcZ, JK, JJ, JI;
+int led1r = 13;
+int led2v = 12;
+int led3a = 4;
+int buz1 = 8;
+
+void setup() {
+  Wire.begin();                   // Inicialización de la biblioteca
+  Wire.beginTransmission(MPU);    // Comenzar transmisión a MPU
+  Wire.write(0x6B);               // registro PWR_MGMT_1
+  Wire.write(0);                  // MPU-6050 al modo de inicio
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+
+//Configara el pin en modo entrada-salida
+  pinMode(led1r, OUTPUT);
+  pinMode(led2v, OUTPUT);
+  pinMode(led3a, OUTPUT);
+  pinMode(buz1, OUTPUT);
+  
+}
+
+void loop() {
+  Wire.beginTransmission(MPU);      // Iniciar Transferencia
+  Wire.write(0x3B);                 // registra 0x3B (ACCEL_XOUT_H), registra datos en cola.
+  Wire.endTransmission(false);      // mantener la conexión
+  Wire.requestFrom(MPU, 14, true);  // Solicitar datos a MPU
+  //Lee byte a byte
+  AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  
+  //Inicializamos nuestra variable
+  JI=AcX;
+  //Condicion para encender led1 y buzzer
+if( JI>=550){
+    digitalWrite(led1r, HIGH);
+    digitalWrite(buz1, LOW);
+} 
+
+//Inicializamos nuestra variable
+ JJ=AcY;
+ //Condicion para encender led2 y buzzer
+if( JJ<=-550){
+    digitalWrite(led2v, HIGH);
+    digitalWrite(buz1, HIGH);
+}
+
+//Inicializamos nuestra variable
+JK=AcZ;
+//Condicion para encender led3 y buzzer
+if( JK>=18000){
+    digitalWrite(led3a, HIGH);
+    digitalWrite(buz1, HIGH);
+}
+
+  //Imprimimos valores en el serial
+  Serial.print(AcX);
+  Serial.print(","); 
+  Serial.print(AcY);
+  Serial.print(","); 
+  Serial.println(AcZ);
+  
+  delay(20);
+  //Se apagan leds y buzzer
+digitalWrite(led1r, LOW);
+digitalWrite(led2v, LOW);
+digitalWrite(led3a, LOW);
+digitalWrite(buz1, LOW);
+}
